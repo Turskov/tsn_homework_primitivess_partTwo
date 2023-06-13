2012b99686b07becf629e8d96672fb84582de493
tsn <turskov.sn@yandex.ru>
2023-06-13 11:58:13 +0300

    Main.java added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите баланс телефона: ");
-        int phoneBalance = in.nextInt();
+        int phoneBalance = 1000;
         System.out.print("Введите сумму пополнения баланса: ");
-        int cash = in.nextInt();
-        in.close();
-
+        int cash = 100;
         int present = 1; // 1 рубль за каждые 100 рублей пополнения
 
 
