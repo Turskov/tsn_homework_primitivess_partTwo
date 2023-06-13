52e23d045cb4a57713fe1c39af87028ca4162ce9
tsn <turskov.sn@yandex.ru>
2023-06-13 14:38:39 +0300

    Main.java added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int phoneBalance = 1000;
-        System.out.print("Введите сумму пополнения баланса: ");
         int cash = 100;
         int present = 1; // 1 рубль за каждые 100 рублей пополнения
 
 
-        if (cash % 100 == 0) System.out.println("Ваша итоговая сумма составит: " +
-                (phoneBalance + (cash + ((cash * present) / 100))));
-        else System.out.println("Ваш итоговый баланс телефона составил: " + (phoneBalance + cash));
+        if (cash % 100 == 0) {
+            System.out.println("Ваша итоговая сумма составит: " +
+                    (phoneBalance + (cash + ((cash * present) / 100))));
+        }
+        else {
+            System.out.println("Ваш итоговый баланс телефона составил: " + (phoneBalance + cash));
+        }
 
     }
 }
