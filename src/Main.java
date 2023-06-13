3a92328c14e8eadba3183ae7736031020634d6b6
tsn <turskov.sn@yandex.ru>
2023-06-13 11:18:38 +0300

    Main.java added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите баланс телефона: ");
-        int phone_balance = in.nextInt();
+        int phoneBalance = in.nextInt();
         System.out.print("Введите сумму пополнения баланса: ");
         int cash = in.nextInt();
         in.close();
@@ -13,16 +13,8 @@ public class Main {
 
 
         if(cash%100==0) System.out.println("Ваша итоговая сумма составит: " +
-                (phone_balance + (cash + ((cash * present) / 100))));
-        else System.out.println("Ваш итоговый баланс телефона составил: " + (phone_balance + cash));
+                (phoneBalance + (cash + ((cash * present) / 100))));
+        else System.out.println("Ваш итоговый баланс телефона составил: " + (phoneBalance + cash));
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 }
